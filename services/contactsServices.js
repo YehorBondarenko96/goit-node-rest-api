b77f8025dc89cbc1278bfd9a27b3214432ae3f67
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-03-12 21:09:29 +0200

    2.0.0-12.03.24

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,45 +1,10 @@
-
-async function getContactById(contactId) {
-    const listString = await fs.readFile(contactsPath, 'utf-8');
-    const list = JSON.parse(listString);
-    const index = list.findIndex(c => c.id === contactId);
-    if(index === -1){
-        return null
-    };
-    const contact = list[index];
-    return contact
-}
-
-async function removeContact(contactId) {
-    const listString = await fs.readFile(contactsPath, 'utf-8');
-    const list = JSON.parse(listString);
-    const index = list.findIndex(c => c.id === contactId);
-    if(index === -1){
-        return null
-    };
-    const [contForDel] = list.splice(index, 1);
-    await fs.writeFile(contactsPath, JSON.stringify(list, null, 4));
-    return contForDel
-}
-
-async function updateContactById(contactId, data) {
-    const listString = await fs.readFile(contactsPath, 'utf-8');
-    const list = JSON.parse(listString);
-    const index = list.findIndex(c => c.id === contactId);
-    if(index === -1){
-        return null
-    };
-    console.log('list[index]: ', list[index]);
-    list[index] = {...list[index], ...data}
-    console.log('list[index]: ', list[index]);
-    await updateContactlist(list);
-    return list[index]
-}
-
 import Contact from "../models/Contact.js";
 
 const listContacts = () => Contact.find();
 const addContact = data => Contact.create(data);
+const getContactById = contactId => Contact.find({ _id: contactId });
+const updateContactById = (contactId, data) => Contact.findByIdAndUpdate({_id: contactId}, data);
+const removeContact = contactId => Contact.findByIdAndDelete(contactId);
 
 export default {
     listContacts,
