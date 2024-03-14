101d12a63409aee8c6651255aaa1bb5bd33bdd59
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-03-14 13:38:32 +0200

    Update contactsSchemas.js

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -4,7 +4,7 @@ export const createContactSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().pattern(new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)).required(),
     phone: Joi.string().pattern(/^[+\-\d()]+$/).required(),
-    favorite: Joi.boolean().required(),
+    favorite: Joi.boolean(),
 })
 
 export const updateContactSchema = Joi.object({
