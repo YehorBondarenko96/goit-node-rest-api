5fce0c36762e22eccd9998ec59d5e8ff484d5933
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-03-21 15:36:08 +0200

    3.0.0-21.03.24

--- routes/authRouter.test.js
+++ routes/authRouter.test.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 import request from "supertest";
 import app from "../app.js";
 
-const { TEST_DB_HOST, PORT = 3000 } = process.env;
+const { DB_HOST, TEST_PORT = 3001 } = process.env;
 
 describe("test /login route", () => {
     let server = null;
 
     beforeAll(async () => {
-            await mongoose.connect(TEST_DB_HOST);
-            server = app.listen(PORT);
+            await mongoose.connect(DB_HOST);
+            server = app.listen(TEST_PORT);
     });
 
     afterAll(async () => {
@@ -23,7 +23,34 @@ describe("test /login route", () => {
             password: "bbj"
         };
 
-        const { statusCode } = await request(app).post("/auth/users/login").send(loginData);
-        expect(statusCode).toBe(201);
+        const { statusCode, body } = await request(app).post("/api/users/login").send(loginData);
+        expect(statusCode).toBe(200);
+        expect(body).toHaveProperty("token");
+        expect(typeof body.token).toBe("string");
+        expect(body).toHaveProperty("user");
+        expect(typeof body.user.email).toBe("string");
+        expect(typeof body.user.subscription).toBe("string");
+    });
+
+    test("test /login with invalid email", async () => {
+        const loginData = {
+            email: "bz@gmail.com",
+            password: "bbj"
+        };
+
+        const { statusCode, body } = await request(app).post("/api/users/login").send(loginData);
+        expect(statusCode).toBe(401);
+        expect(body.message).toBe("Email or password not wrong");
+    });
+
+    test("test /login with invalid password", async () => {
+        const loginData = {
+            email: "b@gmail.com",
+            password: "bbjz"
+        };
+
+        const { statusCode, body } = await request(app).post("/api/users/login").send(loginData);
+        expect(statusCode).toBe(401);
+        expect(body.message).toBe("Email or password not wrong");
     });
 });
